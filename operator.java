86c4258e93196aa3861d46d1d680048168f6989b
Movesky98 <62997633+Movesky98@users.noreply.github.com>
2020-09-12 17:59:27 +0900

    2주차 과제 2-1번입니다.

---
+++ operator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class operator{
+	
+	public static void main(String[] args) {
+		double result = 0;
+		Scanner operate = new Scanner(System.in);
+		System.out.println("연산>>");
+		double a = operate.nextDouble();
+		String operators = operate.next();
+		double b = operate.nextDouble();
+		operate.close();
+		
+		if(operators.equals("+")) {
+			result = a + b;
+			System.out.println(a + "+" + b + "의 계산 결과는 " + result);
+			
+		}else if(operators.equals("-")) {
+			result = a - b;
+			System.out.println(a + "-" + b +"의 계산 결과는 " + result);
+			
+		}else if(operators.equals("*")) {
+			result = a * b;
+			System.out.println(a + "*" + b +"의 계산 결과는 " + result);
+			
+		}else if(operators.equals("/")) {
+			result = a / b;
+			System.out.println(a + "/" + b +"의 계산 결과는 " + result);
+		}
+		
+	}
+}
